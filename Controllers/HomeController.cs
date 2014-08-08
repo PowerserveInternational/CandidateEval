508b09bd7bbf9f881685d6f33e0c283545f8f20e
PowerserveColin <colin@powerserve.net>
2014-08-08 07:59:33 -0400

    added candidate evaluation project
    added candidate evaluation project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace CandidateEval.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<string> supervisees = new List<string>();
+            string cmdText =
+                @"  select 
+                        [Supervisee].ID AS [Supervisee.ID],
+                        [Supervisee].UserName AS [Supervisee.UserName],
+                        [SuperVisee].[Status] AS [Supervisee.Status],
+                        [SuperVisee].[FirstName] AS [Supervisee.FirstName],
+                        [SuperVisee].[LastName] AS [Supervisee.LastName],
+                        [SuperVisee].Lat AS [Supervisee.Lat],
+                        [SuperVisee].Lng AS [Supervisee.Lng]
+                    from Supervisee
+                ";
+
+            //get connection settings from config file
+            string connectionString = ConfigurationManager.ConnectionStrings["db_connection"].ConnectionString;
+
+            // Set up connection/command.
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = new SqlCommand(cmdText, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            supervisees.Add((string)reader["Supervisee.UserName"]);
+                        }
+                    }
+                }
+            }
+            ViewBag.supervisees = supervisees;
+            return View();
+        }
+
+    }
+}
