fe84fa0cf40dc558dd03c4911c43d89c43d0cfaf
powerservecandidate <ccronin@powerserve.net>
2015-03-09 12:39:06 -0400

    added test query to project

---
+++ CandidateEval/CandidateEval/Scripts/testQuery.sql
@@ -0,0 +1,9 @@
+﻿select 
+    [Supervisee].ID AS [Supervisee.ID],
+    [Supervisee].UserName AS [Supervisee.UserName],
+    [SuperVisee].[Status] AS [Supervisee.Status],
+    [SuperVisee].[FirstName] AS [Supervisee.FirstName],
+    [SuperVisee].[LastName] AS [Supervisee.LastName],
+    [SuperVisee].Lat AS [Supervisee.Lat],
+    [SuperVisee].Lng AS [Supervisee.Lng]
+from Supervisee
