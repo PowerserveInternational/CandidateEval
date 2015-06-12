7e52b2b52a8bdb9318f770023bb4f1f4f438eda7
PowerserveCandidate <ccronin@powerserve.net>
2015-06-12 11:37:20 -0400

    Revert "added test query to project"
    This reverts commit fe84fa0cf40dc558dd03c4911c43d89c43d0cfaf.

--- CandidateEval/CandidateEval/Scripts/testQuery.sql
+++
@@ -1,9 +0,0 @@
-﻿select 
-    [Supervisee].ID AS [Supervisee.ID],
-    [Supervisee].UserName AS [Supervisee.UserName],
-    [SuperVisee].[Status] AS [Supervisee.Status],
-    [SuperVisee].[FirstName] AS [Supervisee.FirstName],
-    [SuperVisee].[LastName] AS [Supervisee.LastName],
-    [SuperVisee].Lat AS [Supervisee.Lat],
-    [SuperVisee].Lng AS [Supervisee.Lng]
-from Supervisee
