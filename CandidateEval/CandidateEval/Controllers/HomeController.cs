d91c9d578d195618085ab4c2bbe42babf2801cf5
Christopher McInnis <chris@powerserve.net>
2019-03-19 14:40:24 -0400

    Updated project.

--- CandidateEval/CandidateEval/Controllers/HomeController.cs
+++ CandidateEval/CandidateEval/Controllers/HomeController.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using CandidateEval.Models;
 using System.Web.Mvc;
-using System.Data;
-using System.Configuration;
-using System.Data.SqlClient;
-using System.Web.Configuration;
 
 namespace CandidateEval.Controllers
 {
@@ -14,40 +7,9 @@ namespace CandidateEval.Controllers
     {
         public ActionResult Index()
         {
-            List<string> supervisees = new List<string>();
-            string cmdText =
-                @"  select 
-                        [Supervisee].ID AS [Supervisee.ID],
-                        [Supervisee].UserName AS [Supervisee.UserName],
-                        [SuperVisee].[Status] AS [Supervisee.Status],
-                        [SuperVisee].[FirstName] AS [Supervisee.FirstName],
-                        [SuperVisee].[LastName] AS [Supervisee.LastName],
-                        [SuperVisee].Lat AS [Supervisee.Lat],
-                        [SuperVisee].Lng AS [Supervisee.Lng]
-                    from Supervisee
-                ";
+            ViewBag.Supervisees = Supervisee.GetList();
 
-            //get connection settings from config file
-            string connectionString = ConfigurationManager.ConnectionStrings["db_connection"].ConnectionString;
-
-            // Set up connection/command.
-            using (var connection = new SqlConnection(connectionString))
-            {
-                using (var command = new SqlCommand(cmdText, connection))
-                {
-                    connection.Open();
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            supervisees.Add((string)reader["Supervisee.UserName"]);
-                        }
-                    }
-                }
-            }
-            ViewBag.supervisees = supervisees;
             return View();
         }
-
     }
 }
---
+++ CandidateEval/CandidateEval/Models/Supervisee.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CandidateEval.Models
+{
+    public class Supervisee
+    {
+        private Supervisee(IDataReader dataReader)
+        {
+            ID = (int)dataReader["ID"];
+            UserName = (string)dataReader["UserName"];
+            Status = (string)dataReader["Status"];
+            FirstName = (string)dataReader["FirstName"];
+            LastName = (string)dataReader["UserName"];
+            Lat = (double)dataReader["Lat"];
+            Lng = (double)dataReader["Lng"];
+        }
+
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Status { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+
+        public static List<Supervisee> GetList()
+        {
+            List<Supervisee> supervisees = new List<Supervisee>();
+
+            string cmdText = @"
+                SELECT 
+                    ID,
+                    UserName,
+                    Status,
+                    FirstName,
+                    LastName,
+                    Lat,
+                    Lng
+                FROM
+                    Supervisee";
+            
+            string connectionString = ConfigurationManager.ConnectionStrings["db_connection"].ConnectionString;
+            
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = new SqlCommand(cmdText, connection))
+                {
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Supervisee supervisee = new Supervisee(reader);
+                            supervisees.Add(supervisee);
+                        }
+                    }
+                }
+            }
+
+            return supervisees;
+        }
+    }
+}
